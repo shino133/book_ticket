7700a75c987bb847a105c321fe15dd74afe2d9c7
viethoang205 <cuvanhieu39@gmail.com>
2025-02-24 22:21:18 +0700

    admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -58,7 +58,8 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Yêu cầu của người quản lý</div>
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Yêu cầu của người quản lý
+                            </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $managerRequestsCount }}</div>
                         </div>
                         <div class="col-auto">
