b70b349348ebcd8a7f93a4ba711efafabc91e6b8
Shino303 <locshino123@gmail.com>
2025-02-24 22:38:42 +0700

    Update Room.php

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
 class Room extends Model
 {
-    use HasFactory;
+    /**
+     * Define relationships.
+     */
+    public function shows()
+    {
+        return $this->hasMany(Show::class);
+    }
 }
