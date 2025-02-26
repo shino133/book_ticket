f8b57f9f4c1a7e20bd52510e218faf7d9ab57699
DuyHai1708 <haitdpn00011@gmail.com>
2025-02-26 09:57:12 +0700

    Update dashboard.blade.php

--- resources/views/manager/dashboard.blade.php
+++ resources/views/manager/dashboard.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Phim</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $numOfMovies }}</div>
                         </div>
                         <div class="col-auto">
@@ -24,7 +24,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Chương trình</div>
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Shows</div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $numOfShows }}</div>
                         </div>
                         <div class="col-auto">
