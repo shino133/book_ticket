b6ad28b7243d575152623bad4bb163461750a8f3
Shino303 <locshino123@gmail.com>
2025-02-27 23:03:53 +0700

    Update reservations.blade.php

--- resources/views/user/reservations.blade.php
+++ resources/views/user/reservations.blade.php
@@ -1,32 +1,40 @@
 <div class="container">
-    <h1 class="mt-0 mb-0">Đặt Chỗ Của bạn</h1>
+    <h1 class="mt-0 mb-0">Vé đã đặt</h1>
     <hr>
     @if ($reservations->isNotEmpty())
         <table class="table table-responsive table-striped table-hover">
             <thead class="thead-dark">
                 <tr>
-                    <th scope="col">Movie</th>
-                    <th scope="col">Show time</th>
-                    <th scope="col">Seat number</th>
-                    <th scope="col">Show price</th>
+                    <th scope="col">Phim</th>
+                    <th scope="col">Ngày chiếu</th>
+                    <th scope="col">Thời gian bắt đầu</th>
+                    <th scope="col">Thời gian kết thúc</th>
+                    <th scope="col">Số ghế</th>
+                    <th scope="col">Giá vé</th>
                     <th scope="col"></th>
                 </tr>
             </thead>
             @foreach ($reservations as $reservation)
                 <tr>
                     <th>{{ $reservation->show->movie->title }}</th>
-                    <th>{{ $reservation->show->date->toDayDateTimeString() }}</th>
+                    <th>{{ $reservation->show->date->toDateString() }}</th>
+                    <th>{{ $reservation->show->start_time->toTimeString() }}</th>
+                    <th>{{ $reservation->show->end_time->toTimeString() }}</th>
                     <td>{{ $reservation->seat_number }}</td>
                     <td>{{ $reservation->show->price . ' ' . config('app.currency') }}</td>
-                    @if ($reservation->show->date > Carbon\Carbon::now()->add(3, 'hour'))
-                        <td class="disabled">
+                    @php
+                        $canCancel = $reservation->show->date > now()->addHours(3);
+                    @endphp
+
+                    <td class="{{ $canCancel ? '' : 'disabled' }}">
+                        @if ($canCancel)
                             <form action="{{ route('reservations.destroy', $reservation->id) }}" method="POST">
                                 @csrf
                                 @method('delete')
                                 <input class="btn btn-second text-white" type="submit" value="Cancel">
                             </form>
-                        </td>
-                    @endif
+                        @endif
+                    </td>
                 </tr>
             @endforeach
         </table>
